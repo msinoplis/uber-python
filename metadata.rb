78adcedccfd0037eb9b3f1edcd322327dad093f9
msinoplis <msinoplis@spartaglobal.com>
2018-07-31 14:40:49 +0100

    ouhfwoijfw

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'python'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Micheal'
+maintainer_email 'micheal@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures python'
 long_description 'Installs/Configures python'
-version '0.3.0'
+version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
